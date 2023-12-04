13e8c6e15237429aa33703f0272e07fc999b58a1
Dravsky <129130564+Dravsky@users.noreply.github.com>
2023-12-04 12:33:40 -0700

    Added a lot of stuff.

--- Source/RayTracing/Main.cpp
+++ Source/RayTracing/Main.cpp
@@ -3,6 +3,10 @@
 #include "Random.h"
 #include "Color.h"
 #include "Canvas.h"
+#include "Camera.h"
+#include "Scene.h"
+#include "Material.h"
+#include "Sphere.h"
 
 int main(int argc, char** argv)
 {
@@ -14,14 +18,31 @@ int main(int argc, char** argv)
 
 	Canvas canvas(400, 300, renderer);
 
+	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0, 1 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 70.0f, aspectRatio);
+
+	Scene scene(20); // sky color could be set with the top and bottom color
+	scene.SetCamera(camera);
+
+	// create material
+	auto lambertian = std::make_shared<Lambertian>(color3_t{ 0, 0, 1 });
+	auto metal = std::make_shared<Metal>(color3_t{ 1, 1, 1 }, 0.0f);
+
+	for (int i = 0; i < 20; i++)
+	{
+		std::shared_ptr<Material> material = (random01() < 0.5f) ? std::dynamic_pointer_cast<Material>(lambertian) : std::dynamic_pointer_cast<Material>(metal);
+		auto sphere = std::make_unique<Sphere>(random(glm::vec3{ -6, -6, 0 }, glm::vec3{ 6, 6, -20 }), 1, material);
+		scene.AddObject(std::move(sphere));
+	}
+
+	// render scene
+	canvas.Clear({ 0, 0, 0, 1 });
+	scene.Render(canvas, 10);
+	canvas.Update();
+
 	bool quit = false;
 	while (!quit)
 	{
-		canvas.Clear({ 0, 0, 0, 1 });
-		for (int i = 0; i < 1000; i++) canvas.DrawPoint({ random(0, 400), random(0, 300)}, { random01(), random01(), random01(), 1});
-		canvas.Update();
-
-		renderer.PresentCanvas(canvas);
 		SDL_Event event;
 		SDL_PollEvent(&event);
 		switch (event.type)
@@ -29,7 +50,17 @@ int main(int argc, char** argv)
 		case SDL_QUIT:
 			quit = true;
 			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+			}
+			break;
 		}
+
+		renderer.PresentCanvas(canvas);
 	}
 
 	renderer.Shutdown();
